8121b6e5bd10b10db257514238f1b9a9e42253cd
Peachy-Pie <patrick.e.straw@gmail.com>
2022-12-14 16:59:16 -0800

    added html and css to style page to be manipulated by dom

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let searchBtn = document.getElementById('searchBtn')
+const cityUserValue = document.getElementById('cityFinder')
+
+searchBtn.addEventListener('click', imClicked);
+function imClicked() {
+    console.log('im clicked')
+}
+
+function grabWeather () {
+
+}
